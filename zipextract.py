4a9a66c25ffe2071b32f2ab1abaa8bd0f301cf66
htmlcoder2023 <129872277+htmlcoder2023@users.noreply.github.com>
2024-09-29 15:39:42 -0500

    Update zipextract.py

--- zipextract.py
+++ zipextract.py
@@ -2,9 +2,13 @@ import os
 import zipfile
 folder = '../roms'
 extension = ".zip"
+lists = []
 for item in os.listdir(folder):
     if item.endswith(extension):
+        lists.append(item)
         zipfile.ZipFile(item).extractall()
+for items in range(len(lists)):
+    os.system("del " + lists[items])
 for item in os.listdir(folder):
     if item.endswith(extension):
         zipfile.ZipFile(item).extractall()
