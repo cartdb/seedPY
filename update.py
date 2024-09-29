f98177313ab8191f47728130713cc118c9263ef2
htmlcoder2023 <129872277+htmlcoder2023@users.noreply.github.com>
2024-09-29 15:04:38 -0500

    Add files via upload

---
+++ update.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import random
+if len(sys.argv) != 3:
+    raise Exception("Too many or too little arguments!")
+if os.path.isfile(sys.argv[1]) and sys.argv[1].endswith(".zip"):
+    os.system("python zipextract.py")
+else:
+    raise Exception("Zip file not found!")
+os.system("python print.py " + sys.argv[2] + " " + sys.argv[2] + ".txt")
+file = open(sys.argv[2] + ".txt", "r")
+hashes = []
+while True:
+    line = file.readline()
+    if not line:
+        break
+    line = line.split(" SHA512: ")[0]
+    line = line.replace("\\n", "")
+    hashes.append(line)
+rom = random.choices(hashes)
+os.system("python run.py " + str(os.path.getsize(rom)) + " " + rom)
