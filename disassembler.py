8ca7e6e04dd2ddcfd2e4388a9a61c7171c5e1969
cartdb <129872277+cartdb@users.noreply.github.com>
2024-10-10 06:44:12 -0500

    Update disassembler.py

--- disassembler.py
+++ disassembler.py
@@ -5,61 +5,88 @@ import pathlib
 workdir = os.getcwd()
 if len(sys.argv) != 2:
     raise Exception("disassembler.exe [file]")
-if os.path.isfile("nesasm.exe") == False:
-    raise Exception("nesasm.exe is not installed! Download at https://github.com/ClusterM/nesasm and copy the 2 DLL files and nesasm.exe to " + workdir)
-else:
-    if os.path.isfile(file):
-        file = sys.argv[1]
-    else:
-        raise Exception(file + " not found!")
-byteCount = 0
-fileread = file.read()
-if fileread[0:3] != b'\x4e\x45\x53\x1a' or fileread[7:15] != b'\x00\x00\x00\x00\x00\x00\x00\x00\x00':
-    raise Exception("Not a valid INES ROM file! A valid INES ROM file will have the characters [4E 45 53 1A] followed by [Number of PRG Banks] [Number of CHR Banks] and then followed by zeroes [00 00 00 00 00 00 00 00 00]! Edit the ROM in a hex editor if required.")
-prgbanks = fileread[4:5]
-chrbanks = fileread[5:6]
+file = sys.argv[1]
+if os.path.isfile(file) == False:
+    raise Exception(file + " not found!")
+bytesArr = []
+headerBytes = []
+count = 0
+input = open(file, "rb")
+fileread = input.read()
+bytefour = fileread[4:5]
+bytefour = int.from_bytes(bytefour)
+print("PRG ROM Banks: " + bytefour)
+bytefive = fileread[5:6]
+bytefive = int.from_bytes(bytefive)
+print("CHR ROM Banks: " + bytefive)
 bytesix = fileread[6:7]
-mirroring = 0
-mapper = 0
 bytesix = int.from_bytes(bytesix)
 if bytesix&2**0 != 0:
-    mirroring += 2**0
+    print("Mirroring: Vertically Mirrored")
+else:
+    print("Mirroring: Horizontally Mirrored")
 if bytesix&2**1 != 0:
+    if file.endswith(".nes"):
+        os.remove(file)
     raise Exception("This disassembler does not support ROMs that have battery-backed PRG RAM!")
 if bytesix&2**2 != 0:
+    if file.endswith(".nes"):
+        os.remove(file)
     raise Exception("This disassembler does not support ROMs that have 512-byte trainers!")
 if bytesix&2**3 != 0:
+    if file.endswith(".nes"):
+        os.remove(file)
     raise Exception("This disassembler does not support ROMs that have alternate nametable layouts!")
 if bytesix&2**4 != 0 or bytesix&2**5 != 0 or bytesix&2**6 != 0 or bytesix&2**7 != 0:
+    if file.endswith(".nes"):
+        os.remove(file)
     raise Exception("This disassembler does not support ROMs that use mappers!")
-fileoutput = file.split(".")
-fileoutput = fileoutput[0]
-fileoutput = fileoutput + ".asm"
-fileoutput = open(fileoutput, "w")
-bank = 0
-addrdec = 32768
-addr = hex(addrdec)
+byteseven = fileread[7:8]
+byteseven = int.from_bytes(byteseven)
+if byteseven&2**0 != 0:
+    if file.endswith(".nes"):
+        os.remove(file)
+    raise Exception("This disassembler does not support VS System ROMs!")
+if byteseven&2**1 != 0:
+    if file.endswith(".nes"):
+        os.remove(file)
+    raise Exception("This disassembler does not support Playchoice-10 ROMs!")
+if byteseven&2**2 != 0 and byteseven&2**3 != 0:
+    if file.endswith(".nes"):
+        os.remove(file)
+    raise Exception("This disassembler does not support NES 2.0 ROMs!")
+if byteseven&2**4 != 0 or byteseven&2**5 != 0 or byteseven&2**6 != 0 or byteseven&2**7 != 0:
+    if file.endswith(".nes"):
+        os.remove(file)
+    raise Exception("This disassembler does not support ROMs that use mappers!")
 for byte in pathlib.Path(file).read_bytes():
-    if byteCount == 0 and byte >= 16:
-        if linesWritten == 0:
-            fileoutput.write(".bank " + str(bank))
-            fileoutput.write(".org " + str(addr))
-            linesWritten += 1
-        else:
-            fileoutput.write("\n" + ".bank " + str(bank))
-            fileoutput.write("\n" + ".org " + str(addr))
-            linesWritten += 1
-    elif byteCount == 8000:
-        byteCount = 0
-        bank += 1
-        if linesWritten == 0:
-            fileoutput.write(".bank " + str(bank))
-            fileoutput.write(".org " + str(addr))
-            linesWritten += 1
-        else:
-            fileoutput.write("\n" + ".bank " + str(bank))
-            fileoutput.write("\n" + ".org " + str(addr))
-            linesWritten += 1
+    bytesArr.append(byte)
+    if count < 7:
+        headerBytes.append(byte)
+    count += 1
+del bytesArr[:16]
+if os.path.isfile("asm6f.exe") == False:
+    raise Exception("asm6f.exe is not installed! Download asm6f at https://github.com/freem/asm6f and extract the files to " + workdir)
+byteCount = 0
+if fileread[0:4] != b'\x4e\x45\x53\x1a' or os.path.getsize(file) != (bytefour * 16384) + (bytefive * 8192):
+    print(fileread[0:4])
+    raise Exception("Not a valid INES ROM file! A valid INES ROM file will have the characters [4E 45 53 1A] followed by [Number of PRG Banks] [Number of CHR Banks] and then followed by zeroes [00 00 00 00 00 00 00 00 00]! Edit the ROM in a hex editor if required.")
+input = open(file, "wb")
+if fileread[7:15] != b'\x00\x00\x00\x00\x00\x00\x00\x00\x00':
+    input.write(bytearray(headerBytes))
+    input.write(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+    input.write(bytearray(bytesArr))
+input = open(file, "rb")
+fileread = input.read()
+hash = hashlib.sha512(fileread).hexdigest()
+prgbanks = fileread[4:5]
+chrbanks = fileread[5:6]
+fileoutputname = file.split(".nes")
+fileoutputname = fileoutputname[0]
+fileoutputname = fileoutputname + ".asm"
+fileoutput = open(fileoutputname, "w")
+linesWritten = 0
+for byte in pathlib.Path(file).read_bytes():
     if byte >= 16:
         hexVersion = hex(byte)
     else:
@@ -68,9 +95,15 @@ for byte in pathlib.Path(file).read_bytes():
     instruction = ".DB $" + hexVersion
     if linesWritten == 0:
         fileoutput.write(instruction)
-        linesWritten += 1
     else:
         fileoutput.write("\n" + instruction)
-        linesWritten += 1
-    if byte >= 16:
-        byteCount += 1
+    linesWritten += 1
+fileoutput.close()
+os.system('asm6f.exe "' + fileoutputname + '" "' + file + '"')
+filehash = open(file, "rb")
+filehash = filehash.read()
+filehash = hashlib.sha512(filehash).hexdigest()
+if hash == filehash:
+    print("Disassembling complete.")
+else:
+    print("Disassembling failed.")
